5483f03390e51f306fa617773582049de85b4252
Tim Kendrick <timkendrick@gmail.com>
2015-05-18 23:31:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,153 @@
+# skivvy-factory
+
+> Create Skivvy tasks from Factory templates
+
+## Overview
+
+[Skivvy](https://www.npmjs.com/package/skivvy) is a modular task runner for reusable build systems, and [Factory](https://www.npmjs.com/package/factory) provides a quick and easy template scaffolding tool for Node. `skivvy-factory` unites the two, allowing you to turn your Factory templates into reusable Skivvy tasks.
+
+
+## Installation
+
+```bash
+npm install skivvy-factory --save-dev
+```
+
+
+## Example
+
+### As a local Skivvy task:
+
+```javascript
+var skivvyFactory = require('skivvy-factory');
+
+module.exports = skivvyFactory({
+	description: 'Create a component',
+	template: 'templates/component',
+	placeholders: [
+		{
+			name: 'name',
+			message: 'Component name'
+		}
+	],
+	options: {
+		destination: '<%= environment.paths.components %>',
+		overwrite: true
+	},
+	context: {
+		author: '<%= project.author %>',
+		copyright: '<%= project.copyright %>'
+	}
+});
+```
+
+After saving this to your `skivvy_tasks` folder, you can launch your template by running the following commands (where `create-component.js` is the filename of the task):
+
+```bash
+# Configure the task
+skivvy config --config.paths.components=src/components
+
+# Create a new component
+skivvy run create-component
+
+# Create another new component, hard-coding the
+# "name" value and preventing accidental overwrite:
+skivvy run create-component --config.context.name=foo --config.options.overwrite=false
+```
+
+### Within a Skivvy task package:
+
+```javascript
+var skivvyFactory = require('skivvy-factory');
+
+var path = require('path');
+
+exports.tasks = {
+	'create-component': skivvyFactory({
+		description: 'Create a component',
+		template: path.join(__dirname, 'templates/component'),
+		placeholders: [
+			{
+				name: 'name',
+				message: 'Component name'
+			}
+		],
+		options: {
+			destination: '<%= package.paths.components %>',
+			overwrite: true
+		},
+		context: {
+			author: '<%= package.author %>',
+			copyright: '<%= package.copyright %>'
+		}
+	}),
+	'create-service': skivvyFactory({
+		description: 'Create a service',
+		template: path.join(__dirname, 'templates/service'),
+		placeholders: [
+			{
+				name: 'name',
+				message: 'Service name'
+			}
+		],
+		options: {
+			destination: '<%= package.paths.services %>',
+			overwrite: true
+		},
+		context: {
+			author: '<%= package.author %>',
+			copyright: '<%= package.copyright %>'
+		}
+	})
+};
+
+exports.defaults = {
+	paths: {
+		component: null,
+		service: null
+	},
+	author: '<%= project.author %>',
+	copyright: '<%= project.copyright %>'
+}
+```
+
+After installing this package, you can launch your templates by running the following commands, where `PACKAGE_NAME` is the name of the package:
+
+```bash
+# Configure the package
+skivvy config --package=PACKAGE_NAME --config.paths.components=src/components --config.paths.services=src/services
+
+# Create a new component
+skivvy run create-component
+
+# Create a service
+skivvy run create-service
+
+# Create another new component, hard-coding the
+# "name" value and preventing accidental overwrite:
+skivvy run create-component --config.context.name=foo --config.options.overwrite=false
+```
+
+
+## Usage
+
+### `skivvyFactory(options)`
+
+Create a Skivvy task from a Factory template
+
+#### Options:
+
+| Name | Type | Required | Default | Description |
+| ---- | ---- | -------- | ------- | ----------- |
+| `template` | `string` | Yes | N/A | Path to the Factory template folder |
+| `options.destination` | `string` | Yes | N/A | Destination directory for output files |
+| `placeholders` | `Array` | No | `[]` | Array of [inquirer](https://www.npmjs.com/package/inquirer) prompts used to gather data for injecting into templates |
+| `context` | `object` | No | `{}` | Preset template placeholder values |
+| `options.overwrite` | `boolean` | No | `false` | Whether to overwrite existing files |
+| `description` | `string` | No | `null` | Skivvy task description |
+
+> Values within the `context` and `options` option values can use Skivvy task config [placeholder values](https://github.com/timkendrick/skivvy/blob/master/docs/guide/02-configuring-tasks.md#using-placeholders-in-configuration-values).
+
+#### Returns:
+
+`function` Skivvy task
---
+++ test/spec/skivvyFactory.spec.js
@@ -0,0 +1,233 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var rewire = require('rewire');
+
+var skivvyFactory = rewire('../../lib/skivvyFactory');
+
+chai.use(sinonChai);
+
+describe('skivvyFactory()', function() {
+	var mockFactory = createMockFactory();
+	var mockSkivvy = createMockSkivvy();
+	var unmockFactory;
+
+	before(function() {
+		unmockFactory = skivvyFactory.__set__('factory', mockFactory);
+	});
+
+	afterEach(function() {
+		mockFactory.reset();
+		mockSkivvy.reset();
+	});
+
+	after(function() {
+		if (unmockFactory) {
+			unmockFactory();
+			unmockFactory = null;
+		}
+	});
+
+	function createMockFactory() {
+		var factory = sinon.spy(function(options, context, callback) {
+			return templateFactory;
+		});
+		var templateFactory = sinon.spy(function(options) {
+			return templateInstance;
+		});
+		var templateInstance = {
+			on: sinon.spy(function(event) {
+				return templateInstance;
+			})
+		};
+		templateInstance.addListener = templateInstance.on;
+		factory._templateFactory = templateFactory;
+		templateFactory._instance = templateInstance;
+
+		factory.events = {
+			'COPY_FILE_COMPLETE': 'copyFileComplete'
+		};
+
+		(function(factoryReset) {
+			factory.reset = function() {
+				factoryReset.call(factory);
+				templateFactory.reset();
+				templateInstance.on.reset();
+			};
+		})(factory.reset);
+
+		return factory;
+	}
+
+	function createMockSkivvy() {
+		return {
+			utils: {
+				log: sinon.spy()
+			},
+			reset: function() {
+				this.utils.log.reset();
+			}
+		};
+	}
+
+	it('Should return a task function', function() {
+		var task = skivvyFactory({
+			template: 'my-template'
+		});
+
+		var actual, expected;
+		actual = task;
+		expected = 'function';
+		expect(actual).to.be.a(expected);
+
+		actual = task.length;
+		expected = 1;
+		expect(actual).to.equal(expected);
+
+		actual = task.description;
+		expected = null;
+		expect(actual).to.equal(expected);
+
+		actual = task.defaults;
+		expected = null;
+		expect(actual).to.eql({
+			options: null,
+			context: null
+		});
+	});
+
+	it('Should set task description and defaults', function() {
+		var task = skivvyFactory({
+			template: 'my-template',
+			description: 'Hello, world!',
+			options: {
+				destination: 'src',
+				overwrite: true
+			},
+			context: {
+				foo: 'foo',
+				bar: 'bar'
+			}
+		});
+
+		var actual, expected;
+
+		actual = task.description;
+		expected = 'Hello, world!';
+		expect(actual).to.equal(expected);
+
+		actual = task.defaults;
+		expected = null;
+		expect(actual).to.eql({
+			options: {
+				destination: 'src',
+				overwrite: true
+			},
+			context: {
+				foo: 'foo',
+				bar: 'bar'
+			}
+		});
+	});
+
+	it('should create the template factory immediately', function() {
+		skivvyFactory({
+			template: 'my-template',
+			description: 'Hello, world!',
+			placeholders: [
+				{ name: 'hello', message: 'Hello' },
+				{ name: 'world', message: 'World' }
+			]
+		});
+
+		expect(mockFactory).to.have.been.calledOnce;
+		expect(mockFactory).to.have.been.calledWith({
+			template: 'my-template',
+			placeholders: [
+				{ name: 'hello', message: 'Hello' },
+				{ name: 'world', message: 'World' }
+			]
+		});
+
+		expect(mockFactory._templateFactory).not.to.have.been.called;
+	});
+
+	it('should invoke the template factory when the task is launched, and return the instance', function() {
+		var task = skivvyFactory({
+			template: 'my-template'
+		});
+
+		var config = {
+			options: {
+				destination: 'src',
+				overwrite: true
+			},
+			context: {
+				foo: 'foo',
+				bar: 'bar'
+			}
+		};
+
+		var templater = task.call(mockSkivvy, config);
+
+		var actual, expected;
+		actual = templater;
+		expected = mockFactory._templateFactory._instance;
+		expect(actual).to.equal(expected);
+
+		expect(mockFactory).to.have.been.calledOnce;
+		expect(mockFactory._templateFactory).to.have.been.calledOnce;
+		expect(mockFactory._templateFactory).to.have.been.calledWith(
+			{
+				destination: 'src',
+				overwrite: true
+			},
+			{
+				foo: 'foo',
+				bar: 'bar'
+			}
+		);
+	});
+
+	it('should log file copy events', function() {
+		var task = skivvyFactory({
+			template: 'my-template'
+		});
+		var config = {
+			options: null,
+			context: null
+		};
+
+		task.call(mockSkivvy, config);
+
+		var listenerSpy = mockFactory._templateFactory._instance.on;
+		expect(listenerSpy).to.have.been.calledOnce;
+
+		var actual, expected;
+
+		actual = listenerSpy.firstCall.args.length;
+		expected = 2;
+		expect(actual).to.equal(expected);
+
+		var eventName = listenerSpy.firstCall.args[0];
+		var handler = listenerSpy.firstCall.args[1];
+
+		actual = eventName;
+		expected = 'copyFileComplete';
+		expect(actual).to.equal(expected);
+
+		actual = handler;
+		expected = 'function';
+		expect(actual).to.be.a(expected);
+
+		handler({
+			src: 'src/my-component',
+			dest: 'dest/my-component'
+		});
+
+		expect(mockSkivvy.utils.log).to.have.been.calledWith('File copied: dest/my-component');
+	});
+});
